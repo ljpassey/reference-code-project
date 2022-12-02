5ad06b96fb3e602eeebfd630c4b3ef9998339fc9
Luke Passey <ljpassey@gmail.com>
2022-12-02 16:30:12 -0700

    This commit includes the initialization of my reference code project with useState and useEffect components added.

---
+++ src/components/EffectComponent.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+//! https://beta.reactjs.org/apis/react/useEffect
+
+// ? You need to pass two arguments to useEffect:
+// ? A setup function with setup code that connects to that system.
+// ? It should return a cleanup function with cleanup code that disconnects from that system.
+// ? A list of dependencies including every value from your component used inside of those functions.
+
+// ? React calls your setup and cleanup functions whenever it’s necessary, which may happen multiple times:
+// ? Your setup code runs when your component is added to the page (mounts).
+// ? After every re-render of your component where the dependencies have changed:
+// ? First, your cleanup code runs with the old props and state.
+// ? Then, your setup code runs with the new props and state.
+// ? Your cleanup code runs one final time after your component is removed from the page (unmounts).
+
+const EffectComponent = () => {
+  const [data, setData] = useState({});
+
+  const getData = () => {
+    axios
+      .get("https://www.swapi.tech/api/people/2")
+      .then((res) => {
+        setData(res.data.result.properties);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getData()
+  }, [])
+  return (
+    <div className="card">
+      <h2>useEffect()</h2>
+      <h3>Name: {data.name}</h3>
+    </div>
+  );
+};
+
+export default EffectComponent;
---
+++ src/components/StateComponent.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const StateComponent = () => {
+  const [userInput, setUserInput] = useState("");
+  // ?useState returns an array with exactly two items:
+  // ?The current state of this state variable, initially set to the initial state you provided.
+  // ?The set function that lets you change it to any other value in response to interaction.
+
+  const handleUpdateState = (event) => {
+    setUserInput(event.target.value);
+  };
+  return (
+    <div className="card">
+      <h2>useState()</h2>
+      <h3>State: {userInput}</h3>
+      <input
+        type="text"
+        placeholder="Enter state"
+        onChange={handleUpdateState}
+      />
+      <h2>const [userInput, setUserInput] = useState("")</h2>
+      <p>
+        useState is a React Hook that lets you add a state variable to your
+        component.
+      </p>
+      <p>
+        The convention is to name state variables like [something, setSomething]
+        using array destructuring.
+      </p>
+      <p>
+        useState returns an array with exactly two items: The current state of
+        this state variable, initially set to the initial state you provided.
+        The set function that lets you change it to any other value in response
+        to interaction. To update whats on the screen, call the set function
+        with some next state:
+      </p>
+    </div>
+  );
+};
+
+export default StateComponent;
