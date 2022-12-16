9c6d6d804e3212103edccde8d4be00a402da8530
Luke Passey <ljpassey@gmail.com>
2022-12-16 10:35:41 -0700

    This commit includes database models and associations

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
 //! Imports at the top
-const express = require('express')
-const cors = require('cors')
-const server = express()
-const db = require('./util/database')
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const db = require("./util/database");
+const { User, Product, Cart } = require("./util/models");
 
 //! Middleware
-server.use(express.json())
-server.use(cors())
+server.use(express.json());
+server.use(cors());
+
+//! Associations
+User.hasMany(Cart);
+Cart.belongsTo(User);
+
+Product.hasMany(Cart);
+Cart.belongsTo(Product);
 
 //! Endpoints
 
 
-db.sync()
+
+db.sync();
 //! Listen
-server.listen(4000, () => {console.log('Server is running on Port 4000')})
+server.listen(4000, () => {
+  console.log("Server is running on Port 4000");
+});
